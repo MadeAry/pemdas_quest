2f13b3feb4b272549d29f64b850d4e83187f75aa
MadeAry <110508803+MadeAry@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ pyhtont.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello World")
+print("Nama Saya Made")
+print("Age 15 y.o")
+print("Saya Penyuka Donghua")
+print("Waifu Saya Medusa")
+print("Cita Cita Ke Swist")
+print("Suka Lagu Western")
+print("Pengen Lancar Ngomong Ingrris")
+print("Saya Pendek")
+print("Udah Pernah Glory 1k")  
---
+++ pyhtont2.py
@@ -0,0 +1,12 @@
+# Belajar Input
+
+nama = "Made" # Made adalah data, nama adalah variabel
+umur = 15
+alamat = "cimanggis, Kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
+print("Nama saya adalah " + nama + " Umur saya " +str(umur) +" Alamat saya " + alamat )
