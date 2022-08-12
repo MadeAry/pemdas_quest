2cd3078c75384f557e4889f01ba76ff6eb3a44d1
MadeAry <110508803+MadeAry@users.noreply.github.com>
2022-08-11 20:40:36 -0700

    Add files via upload

---
+++ UPRAK_i Made Arya DW_X TKJ1 .py
@@ -0,0 +1,9 @@
+nama = input("Masukkan nama anda: ")
+
+nama_belakang = input("MAsukkan nama belakang anda: ")
+
+print("Nama anda adalah", nama, "Nama belakang anda adalah", nama_belakang)
+
+suhu = int(input("Masukkan suhu celcius: "))
+reamur =(4/5 * suhu) +32
+print(suhu,"celcius =", reamur , "reamur")
